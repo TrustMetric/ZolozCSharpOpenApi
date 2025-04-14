8768b54ff6524a7e48c6ff85b60078488ae1f64c
Farouk Rizki <k2d6xxh7pf@K2D6XXH7PFs-MacBook-Pro.local>
2025-04-14 09:33:49 +0700

    Added required fields and primary constructors

--- OpenApiClient.cs
+++ OpenApiClient.cs
@@ -2,20 +2,15 @@
 
 namespace ZolozCSharpOpenApi;
 
-public class OpenApiClient
+public class OpenApiClient(string hostUrl, bool encrypted)
 {
-    public string HostUrl {get; init;}
-    public string ClientID {get; set;}
-    public string MerchantPrivateKey {get; set;} 
-	public string OpenApiPublicKey {get; set;}
-	public bool Encrypted {get; set;}
-	public bool IsLoadTest {get; set;}
+    public required string HostUrl { get; init; } = hostUrl;
+    public required string ClientID {get; set;}
+    public required string MerchantPrivateKey {get; set;} 
+	public required string OpenApiPublicKey {get; set;}
+    public bool Encrypted { get; set; } = encrypted;
+    public bool IsLoadTest {get; set;}
 	public int AesLength {get; set;}
-
-    public OpenApiClient (string hostUrl, bool encrypted){
-        HostUrl = hostUrl;
-        Encrypted = encrypted;
-    }
 
     public OpenApiClient (
         string hostUrl,
