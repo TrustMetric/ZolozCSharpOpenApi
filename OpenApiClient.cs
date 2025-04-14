738bef2b0150176c84a92fdcd1f683e61a7b5c43
Farouk Rizki <k2d6xxh7pf@K2D6XXH7PFs-MacBook-Pro.local>
2025-04-14 09:55:17 +0700

    Modified fields to be immutable after init

--- OpenApiClient.cs
+++ OpenApiClient.cs
@@ -2,29 +2,20 @@
 
 namespace ZolozCSharpOpenApi;
 
-public class OpenApiClient(string hostUrl, bool encrypted)
+public class OpenApiClient(
+    string hostUrl,
+    string clientID,
+    string merchantPrivateKey,
+    string openApiPublicKey,
+    bool encrypted,
+    bool isLoadTest,
+    int aesLength)
 {
     public required string HostUrl { get; init; } = hostUrl;
-    public required string ClientID {get; set;}
-    public required string MerchantPrivateKey {get; set;} 
-	public required string OpenApiPublicKey {get; set;}
-    public bool Encrypted { get; set; } = encrypted;
-    public bool IsLoadTest {get; set;}
-	public int AesLength {get; set;}
-
-    public OpenApiClient (
-        string hostUrl,
-        string clientID,
-        string merchantPrivateKey,
-        string openApiPublicKey,
-        bool encrypted,
-        bool isLoadTest,
-        int aesLength) : this(hostUrl, encrypted)
-    {
-        ClientID = clientID;
-        MerchantPrivateKey = merchantPrivateKey;
-        OpenApiPublicKey = openApiPublicKey;
-        IsLoadTest = isLoadTest;
-        AesLength = aesLength;
-    }
+    public required string ClientID { get; init; } = clientID;
+    public required string MerchantPrivateKey { get; init; } = merchantPrivateKey;
+    public required string OpenApiPublicKey { get; init; } = openApiPublicKey;
+    public bool Encrypted { get; init; } = encrypted;
+    public bool IsLoadTest { get; init; } = isLoadTest;
+    public int AesLength { get; init; } = aesLength;
 }
